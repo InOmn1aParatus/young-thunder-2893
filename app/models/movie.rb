e0720bdce099f07bea547a897983676497f07d52
Matt Kragen <mattdkragen@gmail.com>
2021-07-19 10:41:07 -0700

    Add method for avg actor age

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,8 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_actors
   has_many :actors, through: :movie_actors
+
+  def avg_actor_age
+    self.actors.average(:age)
+  end
 end
--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -4,4 +4,31 @@ RSpec.describe Movie do
   describe 'relationships' do
     it { should belong_to(:studio) }
   end
+
+  before :each do
+    @universal = Studio.create(
+      name: 'Universal Studios', location: 'Hollywood'
+    )
+    @raiders = @universal.movies.create(
+      title: 'Raiders of the Lost Ark',
+      creation_year: 1981,
+      genre: 'Action/Adventure'
+    )
+    @cage = Actor.create(
+      name: 'Nic Cage',
+      age: 50
+    )
+    @jill = Actor.create(
+      name: 'Jill Jillian',
+      age: 49
+    )
+    MovieActor.create(actor: @cage, movie: @raiders)
+    MovieActor.create(actor: @jill, movie: @raiders)
+  end
+
+  describe 'methods' do
+    it 'calculates average actor age' do
+      expect(@raiders.avg_actor_age).to eq(49.5)
+    end
+  end
 end
