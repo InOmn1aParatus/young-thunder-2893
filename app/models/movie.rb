41b5e84576f0278dbcd23ab1d7269251d1e06aa7
Matt Kragen <mattdkragen@gmail.com>
2021-07-19 09:12:12 -0700

    Add validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  validates :title, uniqueness: true
   belongs_to :studio
   has_many :actors, through: :movie_actors
 end
--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,3 +1,4 @@
 class Studio < ApplicationRecord
+  validates :name, uniqueness: true
   has_many :movies
 end
