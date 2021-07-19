375ebc7c6c64105cc7ab5e60f04666b2470d921f
Matt Kragen <mattdkragen@gmail.com>
2021-07-19 08:46:19 -0700

    Create relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
 universal = Studio.create(name: 'Universal Studios', location: 'Hollywood')
 universal.movies.create(title: 'Raiders of the Lost Ark', creation_year: 1981, genre: 'Action/Adventure')
 universal.movies.create(title: 'Shrek', creation_year: 2001, genre: 'Comedy')
+
+cage = Actor.create(
+  name: 'Nic Cage',
+  age: 50
+)
+jo = Actor.create(
+  name: 'Scarlett Jo',
+  age: 40
+)
+adam = Actor.create(
+  name: 'Adam Dude',
+  age: 30
+)
